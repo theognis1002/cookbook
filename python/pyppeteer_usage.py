967c5ec6aa489cc690e05b17a31163760dff224c
theognis1002 <theognis1002@gmail.com>
2021-02-11 20:30:32 -0500

    pyppeteer added

---
+++ python/pyppeteer_usage.py
@@ -0,0 +1,17 @@
+import asyncio
+from pyppeteer import launch
+from pyppeteer_stealth import stealth
+
+
+async def main():
+    browser = await launch(headless=True)
+    page = await browser.newPage()
+
+    await stealth(page)  # <-- Here
+
+    await page.goto("https://bot.sannysoft.com/")
+    await page.screenshot({"path": "example.png"})
+    await browser.close()
+
+
+asyncio.get_event_loop().run_until_complete(main())
