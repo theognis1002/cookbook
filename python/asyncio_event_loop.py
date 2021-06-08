741925b5617c12418b1a943efdde2fe1fe04cfcf
theognis1002 <theognis1002@gmail.com>
2021-06-07 22:25:59 -0400

    added tortoise orm and asyncio event loop

---
+++ python/asyncio_event_loop.py
@@ -0,0 +1,36 @@
+import asyncio
+import random
+import time
+
+import aiohttp
+
+from utils.proxies import proxies
+
+start_time = time.time()
+
+
+def sync_func():
+    time.sleep(5)
+    print("sync_func ran")
+
+
+async def make_request(session, url):
+    response = await session.get(url)
+    return await response.json()
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        tasks = [make_request(session, "https://httpbin.org/ip") for _ in range(99)]
+        result = await asyncio.gather(*tasks)
+        print(result)
+
+
+if __name__ == "__main__":
+    sync_func()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    sync_func()
+
+    loop.close()
+    print("--- %s seconds ---" % (time.time() - start_time))
---
+++ python/tortoise_orm.py
@@ -0,0 +1,32 @@
+from tortoise import Tortoise, fields, run_async
+from tortoise import models
+
+
+class Event(models.Model):
+    id = fields.IntField(pk=True)
+    name = fields.TextField()
+    datetime = fields.DatetimeField(null=True)
+
+    class Meta:
+        table = "event"
+
+    def __str__(self):
+        return self.name
+
+
+async def run():
+    await Tortoise.init(db_url="sqlite://sqlite.db", modules={"models": ["__main__"]})
+    await Tortoise.generate_schemas()
+
+    event = await Event.create(name="Test")
+    await Event.filter(id=event.id).update(name="Updated name")
+
+    print(await Event.filter(name="Updated name").first())
+
+    await Event(name="Test 2").save()
+    print(await Event.all().values_list("id", flat=True))
+    print(await Event.all().values("id", "name"))
+
+
+if __name__ == "__main__":
+    run_async(run())
