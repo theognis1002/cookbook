0e617eaeca66ae478f0f276ec7d61d89dfb2af6c
theognis1002 <theognis1002@gmail.com>
2021-09-01 23:14:23 -0400

    added custom context manager

---
+++ python/context_managers.py
@@ -0,0 +1,28 @@
+"""
+Custom context manager
+"""
+from contextlib import contextmanager
+
+def acquire_resource(*args, **kwargs):
+    """setup function"""
+    pass
+
+def release_resource(*args, **kwargs):
+    """teardown function"""
+    pass
+
+
+@contextmanager # builtin decorator
+def managed_resource(*args, **kwds):
+    # Code to acquire resource, e.g.:
+    resource = acquire_resource(*args, **kwds)
+    try:
+        yield resource
+    finally:
+        # Code to release resource, e.g.:
+        release_resource(resource)
+
+with managed_resource(timeout=3600) as resource:
+    pass
+     # Resource is released at the end of this block,
+     # even if code in the block raises an exception
