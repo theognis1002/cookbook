803d5be657fb8461be7463686af14a78c414eb5c
theognis1002 <theognis1002@gmail.com>
2021-09-05 20:24:06 -0400

    added addtnl decorator.py examples and ran linter

--- python/design_patterns/decorator.py
+++ python/design_patterns/decorator.py
@@ -1,4 +1,4 @@
-from functools import wraps
+import functools
 
 """
 Best practice: use functools wraps
@@ -7,7 +7,7 @@ Best practice: use functools wraps
 
 
 def logged(func):
-    @wraps(func)
+    @functools.wraps(func)
     def with_logging(*args, **kwargs):
         print(func.__name__ + " was called")
         return func(*args, **kwargs)
@@ -27,3 +27,42 @@ if __name__ == "__main__":
     print(
         example_func(3)
     )  # prints return value and extra decorator functionality "example_func was called"
+
+
+"""
+cache decorator - func & class implementation
+"""
+
+
+def cached(func):
+    cached_data = {}
+
+    @functools.wraps(func)
+    def cached_dec(*args):
+        try:
+            return cached_data[args]
+        except KeyError:
+            cached_data[args] = ret = func(*args)
+            return ret
+        return cached_dec
+
+
+class cached:
+    def __init__(self, func):
+        self.func = func
+        self.cached_data = {}
+
+        functools.update_wrapper(self, func)
+
+    def __call__(self, *args):
+        try:
+            return self.cached_data[args]
+        except KeyError:
+            self.cached_data[args] = ret = self.func(*args)
+            return ret
+
+
+@cached
+def compute(x: int) -> int:
+    print(f"calling w/ {x}")
+    return x * x
