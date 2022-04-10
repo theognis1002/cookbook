21d6f9c6157736d00bd699db25bce6935c0c6013
theognis1002 <theognis1002@gmail.com>
2022-04-10 00:54:34 -0400

    Lint fix

--- python/context_managers.py
+++ python/context_managers.py
@@ -36,8 +36,9 @@ with managed_resource(timeout=3600) as resource:
 """
 #2) Class-Based
 """
-class FileHandler():
 
+
+class FileHandler:
     def __init__(self, file_name, file_mode):
         self._file_name = file_name
         self._file_mode = file_mode
@@ -46,20 +47,22 @@ class FileHandler():
         self._file = open(self._file_name, self._file_mode)
         return self._file
 
-
-    def __exit__(self, exc_type,exc_value, exc_traceback):
+    def __exit__(self, exc_type, exc_value, exc_traceback):
         self._file.close()
 
-with FileHandler('test.txt', 'w') as f:
-    f.write('Test')
+
+with FileHandler("test.txt", "w") as f:
+    f.write("Test")
 
 
 """
 #3) Decorator
 """
 from contextlib import ContextDecorator
+
+
 class file_handler_mixin(ContextDecorator):
-    def __init__(self,file_name, file_mode):
+    def __init__(self, file_name, file_mode):
         self._file_name = file_name
         self._file_mode = file_mode
         self._file = None
@@ -69,13 +72,15 @@ class file_handler_mixin(ContextDecorator):
         self._file = open(self._file_name, self._file_mode)
         return self._file
 
-    def __exit__(self,exc_type,exc_value,exc_traceback):
+    def __exit__(self, exc_type, exc_value, exc_traceback):
         print(f"Exit Method: File Mode {self._file_mode}")
         self._file.close()
 
-@file_handler_mixin("test.txt","w" )
+
+@file_handler_mixin("test.txt", "w")
 def write_to_file():
     print("Not access to the value returned by the __enter__ method")
+
 
 if __name__ == "__main__":
     write_to_file()
