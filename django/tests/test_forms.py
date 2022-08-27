bbba8bf9020fdc1dad4f9d7ec2e338758af44f51
theognis1002 <theognis1002@gmail.com>
2022-08-27 17:05:19 -0400

    Add django pytest module with multiple examples

---
+++ django/tests/test_forms.py
@@ -0,0 +1,48 @@
+import pytest
+from mixer.backend.django import mixer
+from myapp.forms import MyModelForm
+
+pytestmark = pytest.mark.django_db
+
+
+class TestMyModelForm:
+    def test_mymodelform(self):
+        form = MyModelForm()
+        assert False is form.is_valid()
+
+        data = {"name": "Hans"}
+        form = MyModelForm(data=data)
+        assert False is form.is_valid()
+        assert form.errors
+        assert "other_model" in form.errors, "other_model cant be null"
+
+        other_model = mixer.blend("myapp.MyOtherModel")
+        data = {"name": "Hans", "other_model": other_model.pk}
+        form = MyModelForm(data=data)
+        assert True is form.is_valid()
+        assert not form.errors, "Should be no errors, when form is valid"
+
+    def test_clean(self):
+        other_model = mixer.blend("myapp.MyOtherModel")
+        data = {"name": "Hans", "other_model": other_model.pk}
+        form = MyModelForm(data=data)
+        assert form.is_valid()
+
+        # Test clean_name method
+        data["name"] = "forty two"
+        form = MyModelForm(data=data)
+        assert False is form.is_valid()
+        assert "name" in form.errors
+
+        # Test clean method
+        data["name"] = "42"
+        form = MyModelForm(data=data)
+        assert "__all__" in form.errors
+        assert False is form.is_valid()
+
+        # Test error code in clean method
+        data["name"] = "21"
+        form = MyModelForm(data=data)
+        assert "__all__" in form.errors
+        assert False is form.is_valid()
+        assert form.errors["__all__"].as_data()[0].code == "truth_bending"
---
+++ django/tests/test_views.py
@@ -0,0 +1,121 @@
+import pytest
+from django import urls
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import RequestFactory
+from django.urls import reverse
+from mixer.backend.django import mixer
+from myapp import views
+from myapp.forms import MyModelForm
+from myapp.models import MyModel
+
+pytestmark = pytest.mark.django_db
+
+
+class TestMyView:
+    def test_anonymous(self):
+        req = RequestFactory().get(reverse("myapp:myview"))
+        resp = views.MyView.as_view()(req)
+        assert resp.status_code == 200
+
+
+class TestMyCreateView:
+    def test_authentication(self):
+        req = RequestFactory().get(reverse("myapp:mycreateview"))
+        # req.user = AnonymousUser()
+        resp = views.MyCreateView.as_view()(req)
+        assert resp.status_code == 200, "Everyone can create a MyModel"
+
+    def test_post(self):
+        assert False is MyModel.objects.all().exists()
+        data = {"name": "Hans", "other_model": mixer.blend("myapp.MyOtherModel").pk}
+        req = RequestFactory().post(reverse("myapp:mycreateview"), data=data)
+        resp = views.MyCreateView.as_view()(req)
+        assert resp.status_code == 302, "Should redirect to success url"
+        assert resp.url == "/create_success/"
+        assert MyModel.objects.all().exists()
+        assert MyModel.objects.all()[0].name == "Hans"
+
+
+class TestMyUpdateView:
+    def test_authentication(self):
+        my_model = mixer.blend("myapp.MyModel")
+
+        req = RequestFactory().get(
+            reverse("myapp:myupdateview", kwargs={"pk": my_model.pk})
+        )
+        req.user = AnonymousUser()
+        resp = views.MyUpdateView.as_view()(req, pk=my_model.pk)
+        assert resp.status_code == 302, "You have to be logged in"
+        assert "login" in resp.url
+
+        req.user = mixer.blend(User)
+        resp = views.MyUpdateView.as_view()(req, pk=my_model.pk)
+        assert resp.status_code == 200, "Authenticaiton successfull"
+
+    def test_post(self):
+        my_model = mixer.blend("myapp.MyModel", name="Dieter")
+        data = {"name": "Hans", "other_model": my_model.other_model.pk}
+        req = RequestFactory().post(
+            reverse("myapp:myupdateview", kwargs={"pk": my_model.pk}), data=data
+        )
+        req.user = mixer.blend(User)
+
+        resp = views.MyUpdateView.as_view()(req, pk=my_model.pk)
+        assert resp.status_code == 302, "redirect to success url"
+        assert "/update_success/" in resp.url
+        assert my_model.name == "Dieter"
+        my_model.refresh_from_db()
+        assert my_model.name == "Hans"
+        assert len(MyModel.objects.all()) == 1, "Should be no new objects"
+
+    def test_invalid_data(self):
+        my_model = mixer.blend("myapp.MyModel", name="Dieter")
+        data = {"name": "Hans"}
+        req = RequestFactory().post(
+            reverse("myapp:myupdateview", kwargs={"pk": my_model.pk}), data=data
+        )
+        req.user = mixer.blend(User)
+
+        resp = views.MyUpdateView.as_view()(req, pk=my_model.pk)
+        assert (
+            resp.status_code == 200
+        ), "should not redirect to success url, when data is invalid"
+        assert my_model.name == "Dieter"
+        my_model.refresh_from_db()
+        assert my_model.name == "Dieter", "Name should not have changed"
+        assert len(MyModel.objects.all()) == 1, "Should be no new objects"
+
+    def test_valid_form_data(self):
+        my_model = mixer.blend("myapp.MyModel", name="Dieter")
+        data = {"name": "Hans", "other_model": mixer.blend("myapp.MyOtherModel").pk}
+
+        form = MyModelForm(data=data)
+        assert form.is_valid()
+        req = RequestFactory().post(
+            reverse("myapp:myupdateview", kwargs={"pk": my_model.pk}), data=form.data
+        )
+        req.user = mixer.blend(User)
+
+        resp = views.MyUpdateView.as_view()(req, pk=my_model.pk)
+        assert resp.status_code == 302, "should redirect to success url"
+        assert my_model.name == "Dieter"
+        my_model.refresh_from_db()
+        assert my_model.name == "Hans", "Name should have changed"
+        assert len(MyModel.objects.all()) == 1, "Should be no new objects"
+
+
+@pytest.mark.django_db
+def test_redirect_to_memes_when_logged_in(authenticated_user, client):
+    """Verify we redirect to the memes page when a user is logged in"""
+    url = urls.reverse("home")
+    resp = client.get(url)
+    assert resp.status_code == 302
+    assert resp.url == urls.reverse("meme:memes")
+
+
+def test_redirect_to_marketing_when_logged_out(client):
+    """Verify we redirect to the marketing page when a user is not logged in"""
+    url = urls.reverse("home")
+    resp = client.get(url)
+    assert resp.status_code == 302
+    assert resp.url == urls.reverse("marketing:landing")
