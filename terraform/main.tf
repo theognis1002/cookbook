cdf46ce4e0df1cc00ec2652e0176e351ba9fb06c
theognis1002 <theognis1002@gmail.com>
2022-09-27 22:36:13 -0400

    Added terraform notes

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region                   = "us-east-1"
+  shared_credentials_files = ["/Users/theognis/.aws/credentials"]
+  profile                  = "default"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "test-theognis-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
